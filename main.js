85f3b6eb570c7565e2b8993c52368e153d6e3357
Dave W <dviper@gmail.com>
2017-04-15 16:57:35 -0600

    final touches

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-$('.nav-item').on('click', function(e) {
-  $(this).siblings().removeClass('active')
-  $(this).addClass('active')
+// Prevent Default Link Behavior
+$('a').on('click', function(e) {
   return false;
 })
 
-$('a').on('click', function(e) {
-  return false;
+$('.menu-side-btn').on('mouseover', () => {
+  if (window.innerWidth <= '950') {
+    $('.side-menu').css('transform', 'translateX(0px)')
+  }
+})
+
+$('.side-menu').on('mouseleave', () => {
+  if (window.innerWidth <= '950') {
+    $('.side-menu').css('transform', 'translateX(-200px)')
+  }
+})
+
+$(window).on('resize', () => {
+  if (window.innerWidth >= '950') {
+    $('.side-menu').css('transform', 'translateX(0px)')
+  } else {
+    $('.side-menu').css('transform', 'translateX(-200px)')
+  }
 })
